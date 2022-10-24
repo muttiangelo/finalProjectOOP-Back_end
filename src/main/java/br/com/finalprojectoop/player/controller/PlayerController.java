bc7fa4ccc61eeed418ce52729d02eac40cd0ba78
Ângelo Mutti <angelo.mutti@sergipetec.org.br>
2022-10-23 22:11:12 -0300

    adding the selects from the player entity

--- src/main/java/br/com/finalprojectoop/player/controller/PlayerController.java
+++ src/main/java/br/com/finalprojectoop/player/controller/PlayerController.java
@@ -1,8 +1,23 @@
 package br.com.finalprojectoop.player.controller;
 
+import br.com.finalprojectoop.player.dao.PlayerDao;
+import br.com.finalprojectoop.player.model.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/players")
 public class PlayerController {
 
+    @Autowired
+    private PlayerDao playerDao;
+
+    @GetMapping("/getAll")
+    public List<Player> getAllPlayers() {
+        return playerDao.getAllPlayers();
+    }
 }
--- src/main/java/br/com/finalprojectoop/player/dao/PlayerDao.java
+++ src/main/java/br/com/finalprojectoop/player/dao/PlayerDao.java
@@ -1,4 +1,25 @@
 package br.com.finalprojectoop.player.dao;
 
+import br.com.finalprojectoop.player.model.Player;
+import br.com.finalprojectoop.player.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PlayerDao {
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    public List<Player> getAllPlayers() {
+        return playerRepository.findAll(Sort.sort(Player.class).by(Player::getPlayerGoals).descending());
+    }
+
+    public List<Player> getPlayerByName(String playerName) {
+        return playerRepository.findAll().stream().filter(player -> player.getPlayerName().equals(playerName)).toList();
+    }
+
 }
